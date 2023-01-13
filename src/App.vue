1f0babf38f2c2be083f087c9425db81a87609eda
Peng Liu <pliu@lattice-engines.com>
2023-01-13 22:34:01 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="docker">
+    <!-- 使用了Sass的BEM命名方式来命名css：block__element--Modifier -->
+    <span class="docker__item docker__item--active">
+      <div class="iconfont">&#xf0174;</div>  <!-- 字体图标 -->
+      <div class="docker__title">首页</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe645;</div>
+      <div class="docker__title">购物车</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe643;</div>
+      <div class="docker__title">订单</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe60d;</div>
+      <div class="docker__title">我的</div>
+    </span>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.docker {
+  display: flex; // 启用浮动布局
+  box-sizing: border-box; // 告诉浏览器：你想要设置的边框和内边距的值是包含在 width 内的。
+  position: absolute; // 绝对定位，相对于整个浏览器页面的偏移量
+  padding: 0 .18rem; //左右内边距为18px
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: 1px solid #F1F1F1;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.docker__item { // 此种命名表示是在.docker里面的item
+  flex: 1; // 没有浮动块的伸展设置，设置平均撑满整个剩余空间。
+  text-align: center;
+  .iconfont {
+    margin: .07rem 0 .02rem 0;
+    font-size: .2rem; //设置字体图标的大小
   }
+}
+.docker__item--active {
+  color: #1FA4FC;
+}
+.docker__title {
+  // 对于中文浏览器(英文没有问题)，chrome最小字体限制是12px，这里需要使用transform scale缩小字体为需要的10px
+  font-size: .2rem;
+  transform: scale(.5, .5); //设置缩放比例，是20px的50% = 10px
+  transform-origin: center top; //定义缩放的中心点
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css' // 添加normalize css的功能
+import './style/base.scss' // 导入本项目的基本css
+import './style/iconfont.css' // 导入字体图标css
 
 createApp(App).use(store).use(router).mount('#app')
