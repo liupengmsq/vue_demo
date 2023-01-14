68581af8af4527c09017ae072c3401858e2b8683
Peng Liu <pliu@lattice-engines.com>
2023-01-14 09:25:17 +0800

    重新整理css结构，引入index.scss

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'normalize.css' // 添加normalize css的功能
-import './style/base.scss' // 导入本项目的基本css
-import './style/iconfont.css' // 导入字体图标css
+import './style/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
