95a9b76177f64b9873bf1b379e76718f2bea116f
Peng Liu <pliu@lattice-engines.com>
2023-01-14 09:36:50 +0800

    加入注释

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'normalize.css' // 添加normalize css的功能
-import './style/index.scss'
+import './style/index.scss' // 导入统一的css文件
 
 createApp(App).use(store).use(router).mount('#app')
