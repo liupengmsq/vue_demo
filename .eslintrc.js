5278e07941d605335dc7c4855739c7e10d1f286e
Peng Liu <pliu@lattice-engines.com>
2023-01-24 22:21:09 +0800

    语法检查禁止检查分号

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    // 禁止检查分号
+    semi: 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
